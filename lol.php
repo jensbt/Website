6db3be3e52d6d3cb72076ab5fd22f66e8d91343c
Jens Thomsen <dnamesnej@gmail.com>
2017-03-22 17:50:36 +0100

    First commit

---
+++ lol.php
@@ -0,0 +1,22 @@
+<?php
+     $user = $_GET['username'];
+     $pass = $_GET['password'];
+ 
+     $sqlconnection = mysqli_connect("localhost", "root", "root", "Information");
+     if(mysqli_connect_errno()) { 
+         echo"failed to connect".mysqli_connect_error(); 
+     }
+ 
+     if(isset($user) && isset($pass)) {
+         $query = "SELECT * FROM Picture1 WHERE id = '".$user."' ";
+         $result = mysqli_query($sqlconnection, $query);
+ 
+         if ($result->num_rows == 0) {
+             echo "Nope";
+         } else {
+             while($row = $result->fetch_assoc()) {
+        	 echo  $row["info"];
+    		}
+         }
+     }
+ ?>
